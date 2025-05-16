7f1b42b868070d7b4b4abb77216110efe68269f2
oyatiilo1998 <33777411+oyatiilo1998@users.noreply.github.com>
2025-05-16 21:51:24 +0500

    Update RemoveElement.go

--- Easy/RemoveElement.go
+++ Easy/RemoveElement.go
@@ -4,7 +4,7 @@ func removeElement(nums []int, val int) int {
 	for i := 0; i < len(nums) ; i++ {
 		if nums[i] != val {
 			 nums[resultCount] = nums[i] 
-            resultCount++
+                         resultCount++
 		}
 	}
     
